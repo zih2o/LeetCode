b8502d436e46d4e4b5b2ae5e788733e03cb362da
Jisu Park <85221728+Bibi-Park@users.noreply.github.com>
2022-02-28 23:33:30 +0900

    Time: 474 ms (36.14%), Space: 18.3 MB (27.02%) - LeetHub

---
+++ 706-design-hashmap/706-design-hashmap.py
@@ -0,0 +1,70 @@
+class ListNode:
+     def __init__(self, key=None, value=None):
+        self.key = key
+        self.value = value
+        self.next = None
+        
+class MyHashMap:
+
+    def __init__(self):
+        self.size = 10000
+        self.table = collections.defaultdict(ListNode)
+
+    def put(self, key: int, value: int) -> None:
+        index = key % self.size
+        if self.table[index].value is None:
+            self.table[index] = ListNode(key, value)
+            return
+        
+        p = self.table[index]
+        while p:
+            if p.key == key:
+                p.value = value
+                return
+            elif p.next is None:
+                break
+            p = p.next
+        p.next = ListNode(key, value)
+        return
+    
+    def get(self, key: int) -> int:
+        index = key % self.size
+        
+        # 해당 인덱스가 존재하지 않을때
+        if self.table[index].value is None:
+            return -1
+        
+        #해당 인덱스 존재시
+        p = self.table[index]
+        while p:
+            if p.key == key:
+                return p.value
+            p = p.next
+        return -1
+
+    def remove(self, key: int) -> None:
+        index = key % self.size
+        
+        # 인덱스 존재하지 않는 경우
+        if self.table[index].value is None:
+            return
+        
+        # 삭제할 키값이 첫번째에 존재하는 경우
+        p = self.table[index]
+        if p.key == key:
+            self.table[index] = ListNode() if p.next is None else p.next
+            return
+        
+        # 삭제할 키값이 두번째 이상 노드에 존재하는 경우
+        prev = p
+        while p:
+            if p.key == key:
+                prev.next = p.next
+                return
+            prev, p  = p, p.next
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
