88373f67ebd6bec7fb81dbec56e0277df0fbfd09
Jisu Park <85221728+Bibi-Park@users.noreply.github.com>
2022-03-02 02:18:21 +0900

    Time: 54 ms (24.69%), Space: 14 MB (66.11%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,18 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        def dfs(index: int, path: str):
+            if len(path) == len(digits):
+                    result.append(path)                
+            for i in range(index, len(digits)):
+                for j in dic[digits[i]]:
+                    dfs(i+1, path + j)
+        # 예외처리
+        if not digits:
+            return []
+        
+        result = []
+        dic = {"2" : "abc", "3" : "def", "4" : "ghi", "5" : "jkl",\
+               "6" : "mno", "7" : "pqrs", "8" : "tuv", "9" : "wxyz"}
+        # dfs로 조합찾기
+        dfs(0, "")
+        return result
