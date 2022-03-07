0af2dc7a6b238097c9416c77d3a95f20ccd3d321
Jisu Park <85221728+Bibi-Park@users.noreply.github.com>
2022-03-08 01:42:46 +0900

    Time: 135 ms (85.08%), Space: 15.8 MB (61.29%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -1,14 +1,4 @@
+from itertools import combinations 
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        results = []
-        def dfs(elements, start:int, k:int):
-            if k == 0:
-                results.append(elements[:])
-                return
-            else:
-                for i in range(start, n+1):
-                    elements.append(i)
-                    dfs(elements, i+1, k-1)
-                    elements.pop()
-        dfs([], 1, k)
-        return results
+        return list(combinations(range(1, n+1),k))
